8f606fc13c0244e7a43eb642fc8816ad122b28d5
Julian Pollinger <julian@pollinger.dev>
2026-05-29 10:36:34 +0200

    feat: add control panel for UVNXS Publication settings

---
+++ src/uvnxs/publication/views/controlpanel.py
@@ -0,0 +1,17 @@
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.z3cform import layout
+from uvnxs.publication import _
+from uvnxs.publication.interfaces import IUvnxsPublicationSettings
+
+
+class UvnxsPublicationSettingsEditForm(RegistryEditForm):
+    schema = IUvnxsPublicationSettings
+    schema_prefix = "uvnxs.publication"
+    label = _("label_uvnxs_publication_settings", default="UVNXS Publication Einstellungen")
+
+
+UvnxsPublicationSettingsView = layout.wrap_form(
+    UvnxsPublicationSettingsEditForm,
+    ControlPanelFormWrapper,
+)
