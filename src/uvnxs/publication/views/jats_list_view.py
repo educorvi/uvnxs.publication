0234861a72082bebe9111f6d301c0376f77ccfa7
Nina Mücke <nina.muecke@educorvi.de>
2026-07-20 16:05:49 +0200

    feat: add jats-list-view

---
+++ src/uvnxs/publication/views/jats_list_view.py
@@ -0,0 +1,18 @@
+from edi.jsonforms import _
+from Products.Five.browser import BrowserView
+
+
+class JatsListView(BrowserView):
+    def __call__(self):
+        self.jats_documents = [
+            {
+                "title": doc.title,
+                "subtitle": doc.article_subtitle,
+                "url": doc.getURL(),
+                "article-id": doc.article_id,
+                "pub_date": doc.pub_date_ausgabedatum,
+                "pub_date_updated": doc.pub_date_aktualisierte_fassung,
+            }
+            for doc in self.context.restrictedTraverse("@@contentlisting")()
+        ]
+        return self.index()
