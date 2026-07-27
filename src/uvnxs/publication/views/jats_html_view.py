5ed99d6ea3edb05a0233d2c620b07e6cfd780c50
Nina Mücke <nina.muecke@educorvi.de>
2026-07-27 11:46:51 +0200

    fix: error handling of jats html view

--- src/uvnxs/publication/views/jats_html_view.py
+++ src/uvnxs/publication/views/jats_html_view.py
@@ -6,6 +6,8 @@ from zope.interface import Interface
 
 import jats_importexport_client
 
+from uvnxs.publication import _
+
 
 class IJATSHtmlView(Interface):
     """Marker Interface for IJATSHtmlView"""
@@ -20,7 +22,16 @@ class JATSHtmlView(BrowserView):
     def __call__(self):
         api_instance = jats_importexport_client.ExportApi(get_api_client())
         path = api.content.get_path(self.context, relative=True)
-        self.html = api_instance.export_html(path).dict().get("html", "")
+        try:
+            self.html = api_instance.export_html(path).dict().get("html", "")
+        except jats_importexport_client.exceptions.ServiceException:
+            self.html = _(
+                '<p class="error">Der Artikel kann nicht angezeigt werden, da Front und/oder Body fehlen</p>'
+            )
+        except Exception as e:
+            self.html = _(
+                "<p>Fehler beim Exportieren des Artikels zu HTML: {error}</p>"
+            ).format(error=str(e))
         return self.index()
 
 
