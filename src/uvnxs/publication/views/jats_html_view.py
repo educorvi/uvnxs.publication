3439284b0a05d4c1c44fd6e96bc526af5d06f0d4
jonashuettinger <jonas.huettinger@educorvi.de>
2026-08-17 13:31:15 +0200

    feat: new edit view for jats article

--- src/uvnxs/publication/views/jats_html_view.py
+++ src/uvnxs/publication/views/jats_html_view.py
@@ -17,13 +17,17 @@ class IJATSHtmlRawView(Interface):
     """Marker Interface for IJATSHtmlRawView"""
 
 
+class IJATSHtmlEditView(Interface):
+    """Marker Interface for IJATSHtmlEditView"""
+
+
 @implementer(IJATSHtmlView)
 class JATSHtmlView(BrowserView):
     def __call__(self):
         api_instance = jats_importexport_client.ExportApi(get_api_client())
         path = api.content.get_path(self.context, relative=True)
         try:
-            self.html = api_instance.export_html(path).dict().get("html", "")
+            self.html = self.get_html(api_instance, path)
         except jats_importexport_client.exceptions.ServiceException:
             self.html = _(
                 "The article cannot be displayed in HTML format because"
@@ -34,6 +38,9 @@ class JATSHtmlView(BrowserView):
             self.html = _("Error while exporting the article to HTML.")
         return self.index()
 
+    def get_html(self, api_instance, path):
+        return api_instance.export_html(path).dict().get("html", "")
+
 
 @implementer(IJATSHtmlRawView)
 class JATSHtmlRawView(BrowserView):
@@ -43,3 +50,14 @@ class JATSHtmlRawView(BrowserView):
         html = api_instance.export_html(path).dict().get("html", "")
         self.request.response.setHeader("Content-Type", "text/html; charset=utf-8")
         return html
+
+
+@implementer(IJATSHtmlEditView)
+class JATSHtmlEditView(JATSHtmlView):
+    def get_html(self, api_instance, path):
+        return (
+            api_instance
+            .export_html(path, include_edit_links=True)
+            .dict()
+            .get("html", "")
+        )
