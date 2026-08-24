8efafdcb75289e3a07c010328b61bb28794f224d
Julian Pollinger <julian@pollinger.dev>
2026-08-24 11:13:14 +0200

    feat: add support for collective collectionfilter and faceted search

---
+++ src/uvnxs/publication/collectionfilter.py
@@ -0,0 +1,15 @@
+
+from collective.collectionfilter.interfaces import IGroupByCriteria
+from collective.collectionfilter.interfaces import IGroupByModifier
+from zope.component import adapter
+from zope.interface import implementer
+
+
+@implementer(IGroupByModifier)
+@adapter(IGroupByCriteria)
+def groupby_modifier(groupby):
+    groupby._groupby["fachbereiche"] = {
+        "index": "fachbereiche",
+        "metadata": "fachbereiche",
+        "display_modifier": lambda value, index: value,
+    }
