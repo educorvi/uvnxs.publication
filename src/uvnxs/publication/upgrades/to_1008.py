5c78291251cbecd068b5da4e6774be6a8f52871e
Nina Mücke <nina.muecke@educorvi.de>
2026-08-25 17:01:18 +0200

    feat: migration for last changes on article

---
+++ src/uvnxs/publication/upgrades/to_1008.py
@@ -0,0 +1,45 @@
+from Products.CMFCore.utils import getToolByName
+from plone import api
+
+
+def update_article_metadata(context):
+    """Update the article metadata.
+    - activate plone.relateditems (to extend relatec_articles) and nva.webcode.webcode
+      (to replace webcode attribute) behaviors
+    - migrations for
+        - journal_title and journal_subtitle from Textline to choice fields TODO
+        - move content from custom field "keywords" (list of strings) to
+          plone.categorization "subjects" (tuple of strings)
+        - not necessary for "webcode", as it is the same as the nva.webcode field
+        - apply the plone workflow state based on the custom field
+        "veroeffentlichungsstatus" and remove its content afterwards
+    """
+    setup = getToolByName(context, "portal_setup")
+    setup.runImportStepFromProfile(
+        "profile-uvnxs.publication:default",
+        "typeinfo",
+    )
+
+    all_articles = context.portal_catalog(portal_type="Article")
+    for brain in all_articles:
+        article = brain.getObject()
+
+        # migrate keywords to subject
+        keywords = getattr(article, "keywords", None)
+        if keywords:
+            article.subject = tuple(list(article.subject) + keywords)
+            article.keywords = None
+
+        # migrate veroeffentlichungsstatus to plone workflow state
+        workflow_state_map = {
+            "intern veröffentlicht": "internally_published",
+            "veröffentlicht": "internally_published",  # yes, also internally_published
+            "zurückgezogen": "internal",
+            "privat": "private",
+        }
+        veroeffentlichungsstatus = getattr(article, "veroeffentlichungsstatus", None)
+        if veroeffentlichungsstatus in workflow_state_map:
+            api.content.transition(
+                obj=article, to_state=workflow_state_map[veroeffentlichungsstatus]
+            )
+        article.veroeffentlichungsstatus = None
