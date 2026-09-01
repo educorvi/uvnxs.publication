1666ab58be6872e07f2e22bb06b7fd25a817b91c
jonashuettinger <jonas.huettinger@educorvi.de>
2026-09-01 13:08:22 +0200

    refactor: common view search functions

---
+++ src/uvnxs/publication/views/common_search.py
@@ -0,0 +1,34 @@
+def get_document_for_article(obj):
+    """Return a dictionary representation of the given article object."""
+    pub_date = getattr(obj, "pub_date_ausgabedatum", None)
+    pub_date_updated = getattr(obj, "pub_date_aktualisierte_fassung", None)
+    return {
+        "article_id": obj.article_id or "",
+        "title": obj.title or "",
+        "pub_date": pub_date.isoformat() if pub_date else "",
+        "pub_date_updated": pub_date_updated.isoformat() if pub_date_updated else "",
+        "rubrik": obj.journal_title or "",
+        "fachbereich": obj.fachbereich or "",
+        "sachgebiet": obj.sachgebiet or "",
+        "url": obj.absolute_url(),
+    }
+
+def get_rubriken(documents):
+    """Return a list of unique rubriken from the given documents."""
+    return sorted({doc["rubrik"] for doc in documents if doc["rubrik"]})
+
+def get_fachbereiche(documents):
+    """Return a list of unique fachbereiche from the given documents."""
+    return sorted({doc["fachbereich"] for doc in documents if doc["fachbereich"]})
+
+def get_sachgebiete(documents):
+    """Return a list of sachgebiete with their associated fachbereiche."""
+    sachgebiete = {}
+    for doc in documents:
+        if not (sachgebiet := doc["sachgebiet"]):
+            continue
+        if sachgebiet not in sachgebiete:
+            sachgebiete[sachgebiet] = []
+        if (fachbereich := doc["fachbereich"]) and fachbereich not in sachgebiete[sachgebiet]:
+            sachgebiete[sachgebiet].append(fachbereich)
+    return [{"name": sg, "fachbereiche": fachbereiche} for sg, fachbereiche in sachgebiete.items()]
--- src/uvnxs/publication/views/vur_filter_view.py
+++ src/uvnxs/publication/views/vur_filter_view.py
@@ -4,6 +4,8 @@ from zope.interface import Interface
 
 import json
 
+from uvnxs.publication.views.common_search import get_document_for_article, get_fachbereiche, get_sachgebiete
+
 
 class IVuRFilterView(Interface):
     """Marker Interface for VuR Filter View"""
@@ -11,53 +13,18 @@ class IVuRFilterView(Interface):
 
 @implementer(IVuRFilterView)
 class VuRFilterView(BrowserView):
-    def _document_for(self, doc):
-        pub_date = getattr(doc, "pub_date_ausgabedatum", None)
-        pub_date_updated = getattr(doc, "pub_date_aktualisierte_fassung", None)
-        return {
-            "article_id": doc.article_id or "",
-            "title": doc.title or "",
-            "pub_date": pub_date.isoformat() if pub_date else "",
-            "pub_date_updated": pub_date_updated.isoformat()
-            if pub_date_updated
-            else "",
-            "fachbereich": doc.fachbereich or "",
-            "sachgebiet": doc.sachgebiet or "",
-            "url": doc.absolute_url(),
-        }
-
     def __call__(self):
         documents = [
-            self._document_for(doc)
+            get_document_for_article(doc)
             for doc in (
                 brain.getObject()
                 for brain in self.context.restrictedTraverse("@@contentlisting")()
                 if brain.portal_type == "Article"
             )
         ]
+
         self.documents_json = json.dumps(documents, ensure_ascii=False)
-
-        sachgebiete = {}
-        for doc in documents:
-            if not (sachgebiet := doc["sachgebiet"]):
-                continue
-            if sachgebiet not in sachgebiete:
-                sachgebiete[sachgebiet] = []
-            if (fachbereich := doc["fachbereich"]) and fachbereich not in sachgebiete[
-                sachgebiet
-            ]:
-                sachgebiete[sachgebiet].append(fachbereich)
-        self.sachgebiete_json = json.dumps(
-            [
-                {"name": sg, "fachbereiche": fachbereiche}
-                for sg, fachbereiche in sachgebiete.items()
-            ],
-            ensure_ascii=False,
-        )
-
-        fachbereiche = sorted({
-            doc["fachbereich"] for doc in documents if doc["fachbereich"]
-        })
-        self.fachbereiche_json = json.dumps(fachbereiche, ensure_ascii=False)
-
+        self.sachgebiete_json = json.dumps(get_sachgebiete(documents), ensure_ascii=False)
+        self.fachbereiche_json = json.dumps(get_fachbereiche(documents), ensure_ascii=False)
+        self.count = len(documents)
         return self.index()
