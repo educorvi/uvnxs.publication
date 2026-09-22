9d421b8720fc7859d004eca94404a2b4da7c843f
Julian Pollinger <julian@pollinger.dev>
2026-09-22 15:22:37 +0200

    feat: add TimeoutApiClient with default request timeouts

--- src/uvnxs/publication/views/common.py
+++ src/uvnxs/publication/views/common.py
@@ -2,8 +2,29 @@ from plone import api
 
 import jats_importexport_client
 
+class TimeoutApiClient(jats_importexport_client.ApiClient):
+    def call_api(
+        self,
+        method,
+        url,
+        header_params=None,
+        body=None,
+        post_params=None,
+        _request_timeout=None,
+    ):
+        if _request_timeout is None:
+            _request_timeout = (3.0, 15.0)  # connect, read seconds
 
-def get_api_client():
+        return super().call_api(
+            method,
+            url,
+            header_params=header_params,
+            body=body,
+            post_params=post_params,
+            _request_timeout=_request_timeout,
+        )
+
+def get_api_client() -> jats_importexport_client.ApiClient:
     base_url = api.portal.get_registry_record("uvnxs.publication.base_url")
     if api.portal.get_registry_record("uvnxs.publication.use_api_key"):
         api_key = api.portal.get_registry_record("uvnxs.publication.api_key")
@@ -11,4 +32,4 @@ def get_api_client():
         configuration.api_key["APIKeyHeader"] = api_key
     else:
         configuration = jats_importexport_client.Configuration(host=base_url)
-    return jats_importexport_client.ApiClient(configuration=configuration)
+    return TimeoutApiClient(configuration=configuration)
